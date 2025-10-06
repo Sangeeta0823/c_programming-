4c2336bd7b3cd193513128d1d493d46daae28bc8
Sangeeta0823 <sangeeta@eng.rizvi.edu.in>
2025-10-06 16:31:56 +0530

    Experiment1.C

---
+++ Experiment1.C
@@ -0,0 +1,16 @@
+/*******************************************************************************
+Name : Sangeeta bind
+UIN : 251C021
+Class : civil
+DIV : B
+
+#include <stdio.h>
+
+int main() {
+    printf("Name:\t\tSangeeta Bind\n");
+    printf("Age:\t\t19\n");
+    printf("Class:\t\tFE (Civil Engineering)\n");
+    printf("Division:\tB\n");
+    printf("UIN:\t\t251CO21\n");
+    return 0;
+}
