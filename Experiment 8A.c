c7e73d5e2580f95117f402fc5ce1ab258b3d7cf7
Sangeeta0823 <sangeeta@eng.rizvi.edu.in>
2025-11-29 18:25:27 +0530

    Experiment 8A.c

---
+++ Experiment 8A.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+Name : Sangeeta Bind 
+UIN : 251C021
+Div : B 
+Roll no.:18
+Branch:Civil
+
+*******************************************************************************/
+#include <stdio.h>
+#include <string.h>  
+
+int main() {
+    char str[100];
+    int length;
+    printf("Enter a string: ");
+    gets(str);  
+
+    length = strlen(str);   
+
+    printf("Length of the string = %d\n", length);
+
+    return 0;
+}
